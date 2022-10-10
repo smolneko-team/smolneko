e41aeb6417472348e8f7d9e3e7e98eb7abe25058
Vasily Yenin <vasily.yenin@gmail.com>
2022-10-11 00:31:45 +0700

    Add sql schema migrations

---
+++ migrations/000002_create_figures_table.down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS figures_images;
+
+DROP TABLE IF EXISTS characters_images;
+
+DROP TABLE IF EXISTS figures;
+
+DROP TABLE IF EXISTS characters;
---
+++ migrations/000002_create_figures_table.up.sql
@@ -0,0 +1,56 @@
+CREATE TABLE
+    IF NOT EXISTS characters (
+        id char(21) DEFAULT nanoid(),
+        name jsonb,
+        description jsonb,
+        birth_at DATE,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp(0),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp(0),
+        deleted_at TIMESTAMPTZ,
+        is_draft BOOLEAN NOT NULL DEFAULT false,
+        PRIMARY KEY (id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS figures (
+        id char(21) DEFAULT nanoid(),
+        character_id char(21),
+        name VARCHAR(255) NOT NULL,
+        description jsonb,
+        type VARCHAR(255),
+        size VARCHAR(30),
+        height SMALLINT,
+        materials VARCHAR(100) [],
+        release_date DATE,
+        manufacturer VARCHAR(150),
+        links jsonb,
+        price VARCHAR(50) [],
+        created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp(0),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp(0),
+        deleted_at TIMESTAMPTZ,
+        is_draft BOOLEAN NOT NULL DEFAULT false,
+        PRIMARY KEY (id),
+        CONSTRAINT fk_characters FOREIGN KEY (character_id) REFERENCES characters (id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS figures_images (
+        id char(21) DEFAULT nanoid(),
+        path VARCHAR(255) [] NOT NULL,
+        figure_id char(21) UNIQUE NOT NULL,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp(0),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp(0),
+        PRIMARY KEY (id),
+        CONSTRAINT fk_figures FOREIGN KEY (figure_id) REFERENCES figures (id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS characters_images (
+        id char(21) DEFAULT nanoid(),
+        path VARCHAR(255) [] NOT NULL,
+        character_id char(21) UNIQUE NOT NULL,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp(0),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp(0),
+        PRIMARY KEY (id),
+        CONSTRAINT fk_characters FOREIGN KEY (character_id) REFERENCES characters (id)
+    );
