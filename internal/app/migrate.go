9170d2935d72f2cbdf9fe509d363d38934a73f36
Vasily Yenin <vasily.yenin@gmail.com>
2022-12-01 01:53:10 +0700

    Refactor for auto migrations with docker

--- internal/app/migrate.go
+++ internal/app/migrate.go
@@ -3,63 +3,83 @@
 package app
 
 import (
-    "errors"
-    "log"
-    "os"
-    "time"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
-    "github.com/golang-migrate/migrate/v4"
-    // migration
-    _ "github.com/golang-migrate/migrate/v4/database/postgres"
-    _ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 const (
-    _defaultAttempts = 20
-    _defaultTimeout  = time.Second
+	_defaultAttempts = 20
+	_defaultTimeout  = time.Second
 )
 
-// TODO auto migrations
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	SSLMode  string
+}
 
 func init() {
-    databaseURL, ok := os.LookupEnv("PG_URL")
-    if !ok || len(databaseURL) == 0 {
-        log.Fatalf("migrate: environment variable not declared: PG_URL")
-    }
+	cfg := DBConfig{
+		Host:     getEnv("DB_HOST"),
+		Port:     getEnv("DB_PORT"),
+		User:     getEnv("DB_USER"),
+		Password: getEnv("DB_PASSWORD"),
+		Name:     getEnv("DB_NAME"),
+		SSLMode:  getEnv("DB_SSL_MODE"),
+	}
 
-    databaseURL += "?sslmode=disable"
+	url := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Name, cfg.SSLMode,
+	)
 
-    var (
-        attempts = _defaultAttempts
-        err      error
-        m        *migrate.Migrate
-    )
+	var attempts = _defaultAttempts
+	var err error
+	var m *migrate.Migrate
+	for attempts > 0 {
+		m, err = migrate.New("file://migrations", url)
+		if err == nil {
+			break
+		}
 
-    for attempts > 0 {
-        m, err = migrate.New("file://migrations", databaseURL)
-        if err == nil {
-            break
-        }
+		log.Printf("Migrate: postgres is trying to connect, attemps left: %d", attempts)
+		time.Sleep(_defaultTimeout)
+		attempts--
+	}
 
-        log.Printf("Migrate: postgres is trying to connect, attemps left: %d", attempts)
-        time.Sleep(_defaultTimeout)
-        attempts--
-    }
+	if err != nil {
+		log.Fatalf("Migrate: postgres connect error: %s", err)
+	}
 
-    if err != nil {
-        log.Fatalf("Migrate: postgres connect error: %s", err)
-    }
+	err = m.Up()
+	defer m.Close()
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatalf("Migrate: up error: %s", err)
+	}
 
-    err = m.Up()
-    defer m.Close()
-    if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-        log.Fatalf("Migrate: up error: %s", err)
-    }
+	if errors.Is(err, migrate.ErrNoChange) {
+		log.Printf("Migrate: no change")
+		return
+	}
 
-    if errors.Is(err, migrate.ErrNoChange) {
-        log.Printf("Migrate: no change")
-        return
-    }
+	log.Printf("Migrate: up success")
+}
 
-    log.Printf("Migrate: up success")
+func getEnv(key string) string {
+	if env, ok := os.LookupEnv(key); ok && len(env) != 0 {
+		return env
+	} else {
+		log.Fatalf("Migrate: environment variable not declared: %s", key)
+	}
+	return ""
 }
